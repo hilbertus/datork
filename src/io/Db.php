1bcbf7b0468e82fba8b9f2192321a6a0ec0be1c8
Alexander Tichonow <alexander.tichonow@gmail.com>
2016-06-26 18:31:06 +0200

    simple db commands

--- src/io/Db.php
+++ src/io/Db.php
@@ -7,4 +7,70 @@ namespace datork\io;
 class Db
 {
 
+    /**
+     * @param \PDO $pdo
+     * @param string $sql optional with integrated params e. g. UPDATE  user SET color = green WHERE name = :name;
+     * @param array $params optional params e. g. [':name' => 'Hulk']
+     * @return bool
+     */
+    public static function execute($pdo, $sql, $params = [])
+    {
+        $statement = $pdo->prepare($sql);
+        return $statement->execute($params);
+    }
+
+    /**
+     * @param \PDO $pdo
+     * @param string $sql optional with integrated params e. g. SELECT * FROM user WHERE name = :name;
+     * @param array $params optional params e. g. [':name' => 'Hulk']
+     * @param null|string $indexResultWithColumn
+     * @return array
+     */
+    public static function query($pdo, $sql, $params = [], $indexResultWithColumn = null)
+    {
+        $statement = $pdo->prepare($sql);
+        $statement->execute($params);
+        $res = [];
+        while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+            if($indexResultWithColumn !== null){
+                $res[$row[$indexResultWithColumn]] = $row;
+            } else {
+                $res[] = $row;
+            }
+        }
+        return $res;
+    }
+
+//    /**
+//     * @param \PDO $pdo
+//     * @param string $tableName
+//     * @param array $conditions e. g. ['name' => 'Hulk', 'gender' => ['m', 'f']] will be generate folowing SQL: <br>
+//     *      SELECT * FROM $tableName WHERE name = 'Hulk' AND gender IN ('m', 'f');
+//     *
+//     */
+//    public static function autoQuery($pdo, $tableName, $conditions = [])
+//    {
+//
+//    }
+//
+//    /**
+//     * @param \PDO $pdo
+//     * @param string $tableName
+//     * @param string $referencedColumnName
+//     * @param array $data
+//     */
+//    public static function autoUpdate($pdo, $tableName, $referencedColumnName, $data)
+//    {
+//
+//    }
+//
+//    /**
+//     * @param \PDO $pdo
+//     * @param string $tableName
+//     * @param array $data
+//     */
+//    public static function autoInsert($pdo, $tableName, $data)
+//    {
+//
+//    }
 }
--- tests/unit/io/DbTest.php
+++ tests/unit/io/DbTest.php
@@ -8,21 +8,21 @@ use datork\system\File;
 
 class DbTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \PDO
+     */
     private $pdo;
+    /**
+     * @var string
+     */
     private $dbPath;
 
     protected function setUp()
     {
-        $this->dbPath = dirname(dirname(__DIR__)).'/runtime/testDataBase.db';
-        $this->pdo = new \PDO('sqlite:'.$this->dbPath);
+        $this->dbPath = dirname(dirname(__DIR__)) . '/runtime/testDataBase.db';
+        File::delete([$this->dbPath]);
+        $this->pdo = new \PDO('sqlite:' . $this->dbPath);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $sql = "CREATE TABLE `score` (
-            `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            `name`	TEXT NOT NULL,
-            `score`	INTEGER NOT NULL,
-            `description`	TEXT
-        );";
-//        $this->pdo->exec($sql)->
     }
 
     protected function tearDown()
@@ -31,9 +31,64 @@ class DbTest extends \PHPUnit_Framework_TestCase
         File::delete([$this->dbPath]);
     }
 
-
-    public function testExecute()
+    public function testAllFunctions()
     {
-//        $dbh = new PDO('sqlite:/tmp/foo.db');
+        $this->createTable();
+        $this->writeWithExec();
+        $this->query();
+        $this->autoWrite();
+    }
+
+    private function createTable()
+    {
+        $sql = "CREATE TABLE `score` (
+            `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            `name`	TEXT NOT NULL,
+            `score`	INTEGER NOT NULL,
+            `description`	TEXT
+        );";
+        $this->assertTrue(Db::execute($this->pdo, $sql));
+
+
+        $result = $this->pdo->query("SELECT name FROM sqlite_master WHERE type='table' AND name='score';")
+            ->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertEquals(1, count($result));
+    }
+
+    private function writeWithExec()
+    {
+        $sql = "INSERT INTO `score` (`name`, `score`, `description`) 
+                VALUES (:name, :score, :description);";
+        $params = [':name' => 'X-MEN', ':score' => 8, ':description' => 'Darwin.'];
+        $this->assertTrue(Db::execute($this->pdo, $sql, $params));
+
+        $result = $this->pdo->query("SELECT COUNT(*) as count FROM `score` WHERE 1 = 1;")
+            ->fetchAll(\PDO::FETCH_ASSOC);
+        $this->assertEquals(1, $result[0]['count']);
+    }
+
+    private function query()
+    {
+        $sql = "SELECT * FROM score WHERE name = :name;";
+        $res = Db::query($this->pdo, $sql, [':name' => 'X-MEN'], 'id');
+        $expected = [1 => ['id' => 1, 'name' => 'X-MEN', 'score' => 8, 'description' => 'Darwin.']];
+        $this->assertEquals($expected, $res);
+    }
+
+    private function autoWrite()
+    {
+//        $data = [
+//            ['id' => 2, 'name' => 'Deadpool', 'score' => 7, 'description' => 'Funny'],
+//            ['id' => 3, 'name' => 'Hulk', 'score' => 8, 'description' => 'Very green'],
+//            ['id' => 4, 'name' => 'Iron Man', 'score' => 9, 'description' => 'Technical genius']
+//        ];
+    }
+
+    private function getTestData()
+    {
+        return [
+            ['id' => 1, 'name' => 'X-MEN', 'score' => 8, 'description' => 'Darwin.'],
+
+        ];
     }
 }
